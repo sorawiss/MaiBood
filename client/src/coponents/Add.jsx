48e72a093216ede6a3aeb46676b43e28615ddde3
Peasti <sirapeet@gmail.com>
2025-04-20 16:05:28 +0700

    Feat: fill the blank add component to full page

--- client/src/coponents/Add.jsx
+++ client/src/coponents/Add.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState }  from 'react'
+import '../section/style/Add.css'
 
 function Add() {
     return (
-      <div>Add</div>
+      <div className='overall min-h-screen bg-white-bg w-full flex '>
+
+        <div className="add-wrapper">
+
+          <div className="sell-fridge">
+            <div className="text-wrapper">
+              <p className='sell'>ขาย</p>
+              <p className='fridge'>ใส่ตู้เย็น</p>
+            </div>
+            <div className="slide-bar" ></div>
+          </div>
+
+          <div className="details">
+            <div className="food-details">
+              <div className="add-detail">
+                <input
+                  type="text"
+                  placeholder="ใส่ชื่ออาหาร..."
+                  className="foodname-input"
+                />
+              </div>
+              <div className="add-detail">
+                <input
+                  type="text"
+                  placeholder="วันหมดอายุ"
+                  className="exp-input"
+                />
+              </div>
+              <div className="add-detail">
+                <input
+                  type="text"
+                  placeholder="ประเภท"
+                  className="category-input"
+                />
+              </div>
+            </div>
+            <div className="price">
+              <input
+                type="text"
+                placeholder="ราคา (ใส่ 0 บาทได้)"
+                className="price-input"
+              />
+            </div>
+            <div className="post">
+              <p className='add-post'>ลงประกาศ</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     )
   }
   
