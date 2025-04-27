ac6bf5f9acfbce925796f6643d603e38965faaff
sorawiss <sorawit.bunnee@gmail.com>
2025-04-27 10:21:17 +0700

    Refactor : log user only first time

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -1,14 +1,19 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from './AuthContext';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
 
-    if (loading) return <div>Loading...</div>;
-    if (!user) return <Navigate to="/login" />;
 
-    console.log(user);
+    useEffect(() => {
+        console.log(user);
+    }, [user, loading])
+
+
+    if (loading) return <div>Loading...</div>;
+    if (!user) return <Navigate to="/login" replace />;
+    
     
     return children;
 };
