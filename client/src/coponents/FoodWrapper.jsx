e1912b8f787f23f18a696a76f6c0eb93f2982667
sorawiss <sorawit.bunnee@gmail.com>
2025-04-29 00:49:47 +0700

    Feat : food inpost can fetched

--- client/src/coponents/FoodWrapper.jsx
+++ client/src/coponents/FoodWrapper.jsx
@@ -1,34 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import thaiDate from '../lib/thaiDate'
 
 import '../section/style/Home.css'
 
-function FoodWrapper( { exp, price, name, location, image } ) {
+function FoodWrapper({ id, exp, price, name, location, image }) {
     const time = new Date(exp)
     console.log(image)
 
-    
+
     return (
-        <div className="food-container">
-            <div className="food-pic">
-                <img src={image} loading='lazy' ></img>
-                <div className="detail">
-                    <div className="exp-date">
-                        <p className='exp'> {thaiDate(time)} </p>
-                    </div>
-                    <div className="space-for-price">
-                        <div className="price">
-                            <p className='price-number'> {price} </p>
+        <Link to={'/inpost/' + id} >
+            <div className="food-container">
+                <div className="food-pic">
+                    <img src={image} loading='lazy' ></img>
+                    <div className="detail">
+                        <div className="exp-date">
+                            <p className='exp'> {thaiDate(time)} </p>
+                        </div>
+                        <div className="space-for-price">
+                            <div className="price">
+                                <p className='price-number'> {price} </p>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className="food-description-wrapper">
+                    <p className="food-name"> {name} </p>
+                    <p className="food-seller"> {location} </p>
+                </div>
             </div>
-            <div className="food-description-wrapper">
-                <p className="food-name"> {name} </p>
-                <p className="food-seller"> {location} </p>
-            </div>
-        </div>
+        </Link>
+
     )
 }
 
--- server/routes/getFood.js
+++ server/routes/getFood.js
@@ -5,12 +5,39 @@ import pool from '../util/db.js';
 const router = express.Router();
 
 
+// Get All Foods
 router.get('/get-food', async (req, res) => {
     let connection;
     try {
         connection = await pool.getConnection();
         const [result] = await connection.query('SELECT * FROM fridge WHERE is_store = true');
         res.json(result);
+
+    }
+    catch (error) {
+        res.status(500).json({message: 'Internal server error', error: error.message });
+    }
+    finally {
+        if (connection) {
+            try {
+                await connection.release();
+            } catch (releaseError) {
+                console.error('Error releasing database connection:', releaseError);
+            }
+        }
+    }
+})
+
+
+// Get Inpost Food
+router.get('/get-inpost/:id', async (req, res) => {
+    const { id } = req.params;
+
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const [result] = await connection.query('SELECT * FROM fridge WHERE is_store = true AND id = ?', [id] );
+        res.json(result[0]);
 
     }
     catch (error) {
