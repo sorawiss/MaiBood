2b121b8177f4225dc8e29c7cddb5516ebcaf1a25
sorawiss <sorawit.bunnee@gmail.com>
2025-05-14 09:08:13 +0700

    Fix : logout cookies in production

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -208,7 +208,11 @@ router.get('/api/authentication', async (req, res) => {
 
 // Logout
 router.post('/api/logout', (req, res) => {
-    res.clearCookie('token');
+    res.clearCookie('token', {
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,
+    });
     res.json({ message: 'Logged out' });
 });
 
