cd78dfbf8a52aef1adc9ddc0fec4c579e1d6c4bd
sorawiss <sorawit.bunnee@gmail.com>
2025-05-20 21:59:57 +0700

    Fix : from sell to share

--- client/src/coponents/FridgeList.jsx
+++ client/src/coponents/FridgeList.jsx
@@ -101,7 +101,7 @@ function FridgeList({ material, exp, id, isStore }) {
             rounded-[1rem] flex justify-center items-center `}
                 onClick={isStore ? null : handdleSell}
             >
-                {isExpiringSoon(exp) != 2 && <p>{isStore ? 'ขายอยู่' : 'ขาย'}</p>}
+                {isExpiringSoon(exp) != 2 && <p>{isStore ? 'อยูระหว่างแบ่งปัน' : 'แบ่งปัน'}</p>}
 
             </div>
 
