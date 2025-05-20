a1728f32b0ba93a0bbc00128444e407a990bd2b3
sorawiss <sorawit.bunnee@gmail.com>
2025-05-20 16:09:10 +0700

    Feat : connect history with backend

--- client/src/coponents/HistoryList.jsx
+++ client/src/coponents/HistoryList.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 
-function HistoryList() {
-  return (
-    <div className='history-list-container w-full bg-aceent rounded-[16px]  flex flex-col items-center justify-center ' >
-        <h2>อกไก่</h2>
-        <p className='p2 text-secondary' >หมดอายุ 29-04-2025</p>
-        <p className='p2 text-secondary' >แบ่งปันเมื่อ 29-04-2025</p>
+function HistoryList({ material, expireDate, status }) {
+  const date = new Date(expireDate)
 
-        
+  switch (status) {
+    case 2:
+      status = 'กินเอง'
+      break
+    case 3:
+      status = 'แบ่งปัน'
+      break
+    default:
+      status = 'หมดอายุ'
+      break
+  }
+
+
+  return (
+    <div className={`history-list-container w-full ${status === 'กินเอง' ? 'bg-background' : 'bg-aceent'} rounded-[16px]  flex flex-col items-center justify-center`} >
+      <h2>{material}</h2>
+      <p className='p2 text-secondary' >หมดอายุเมื่อ {date.toLocaleDateString('th-TH',
+        { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+      <p className='p2 text-secondary' >{status}</p>
+
+
     </div>
   )
 }
--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -1,17 +1,46 @@
 import React from 'react'
+import { useQuery } from '@tanstack/react-query';
 
+import Loading from '../coponents/Loading'
 import HistoryList from '../coponents/HistoryList'
 
 import history from '/svg/history.svg'
 
+
+const baseURL = import.meta.env.VITE_BASE_URL;
+// http://localhost:8080/api/history
+async function getHistory() {
+    const response = await fetch(`${baseURL}/history`, {
+      method: 'GET',
+      credentials: 'include'
+    })
+    const data = await response.json()
+    return data
+}
+
+
+// Main Component
 function History() {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['history'],
+        queryFn: getHistory
+    })
+
+    if (isLoading) return <Loading />
+    if (error) return <div>Error: {error.message}</div>
+
+    console.log(data)
+
+    
   return (
     <div className='history-page w-full' >
         <div className='flex flex-col items-center justify-center py-[5rem] gap-[2rem]'>
             <img src={history} alt="history" />
 
-            <div className='history-list w-full'>
-                <HistoryList />
+            <div className='history-list w-full flex flex-col gap-[1rem]'>
+              {data.map((item) => (
+                <HistoryList key={item.id} material={item.material} expireDate={item.exp} status={item.is_store} />
+              ))}
             </div>
 
         </div>
